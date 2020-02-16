f379ba7317a6e2c985556a9f05f14c14b6639100
Masahiro Furudate <178inaba.git@gmail.com>
2020-02-16 22:27:46 +0900

    Using secret manager

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/178inaba/totp-webhookbot
 go 1.13
 
 require (
+	cloud.google.com/go v0.53.0
 	github.com/gorilla/websocket v1.4.1 // indirect
 	github.com/hgfischer/go-otp v1.0.0
 	github.com/nlopes/slack v0.6.0
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
+	google.golang.org/genproto v0.0.0-20200212174721-66ed5ce911ce
 )
--- totp.go
+++ totp.go
@@ -3,15 +3,62 @@ package totp
 import (
 	"context"
 	"encoding/hex"
+	"fmt"
+	"os"
 	"time"
 
+	secretmanager "cloud.google.com/go/secretmanager/apiv1beta1"
 	"github.com/hgfischer/go-otp"
 	"github.com/nlopes/slack"
 	"golang.org/x/xerrors"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1beta1"
 )
 
+type secretRepository struct {
+	client *secretmanager.Client
+
+	projectID string
+}
+
+func newSecretRepository(ctx context.Context, projectID string) (*secretRepository, error) {
+	c, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return nil, xerrors.Errorf("new secret manager client: %w", err)
+	}
+
+	return &secretRepository{client: c, projectID: projectID}, nil
+}
+
+func (r *secretRepository) get(ctx context.Context, secretName string) (string, error) {
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: fmt.Sprintf("projects/%s/secrets/%s/versions/latest", r.projectID, secretName),
+	}
+
+	resp, err := r.client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		return "", xerrors.Errorf("get secret: %w", err)
+	}
+
+	return string(resp.Payload.Data), nil
+}
+
 func PostTOTP(ctx context.Context, _ struct{}) error {
-	bs, err := hex.DecodeString("TODO secret")
+	c, err := newSecretRepository(ctx, os.Getenv("GCP_PROJECT"))
+	if err != nil {
+		return xerrors.Errorf("new secret repository: %w", err)
+	}
+
+	secret, err := c.get(ctx, os.Getenv("TOTP_SECRET_SECRET_ID"))
+	if err != nil {
+		return xerrors.Errorf("get secret: %w", err)
+	}
+
+	webhookRawurl, err := c.get(ctx, os.Getenv("TOTP_SLACK_WEBHOOK_URL_SECRET_ID"))
+	if err != nil {
+		return xerrors.Errorf("get slack webhook url: %w", err)
+	}
+
+	bs, err := hex.DecodeString(secret)
 	if err != nil {
 		return xerrors.Errorf("decode secret: %w", err)
 	}
@@ -28,7 +75,7 @@ func PostTOTP(ctx context.Context, _ struct{}) error {
 		Text:      totp.Get(),
 	}
 
-	if err := slack.PostWebhook("TODO url", &wm); err != nil {
+	if err := slack.PostWebhook(webhookRawurl, &wm); err != nil {
 		return xerrors.Errorf("post webhook: %w", err)
 	}
 
